e6f4390db90ba2defaee687bde549d3506d3d2fb
魏林 <10196822@qq.com>
2019-01-24 09:53:25 +0800

    增加log时间目录，并判断当前目录是否存在

--- autotestframework20190108/automation_test_framework/framework/logger.py
+++ autotestframework20190108/automation_test_framework/framework/logger.py
@@ -16,9 +16,22 @@ class Logger(object):
         self.logger.setLevel(logging.DEBUG)
 
         # 创建一个handler，用于写入日志文件
+        #log文件夹名称 如201901240948
         rq = time.strftime('%Y%m%d%H%M', time.localtime(time.time()))
+        #系统当前时间目录 如2019-01-24
+        directory_time = time.strftime("%Y-%m-%d", time.localtime(time.time()))
         # log_path = os.path.dirname(os.getcwd()) + '/Logs/'  # 项目根目录下/Logs 保存日志
-        log_path = os.path.dirname(os.path.abspath('.')) + '/logs/'
+        log_path = os.path.dirname(os.path.abspath('.')) + '/logs/' + directory_time + '\\'
+
+        #判断当前系统日期的文件夹目录是否存在
+        try:
+            if not os.path.exists(log_path):
+                os.makedirs(log_path)
+                print("log目录新建成功：%s" % log_path)
+            else:
+                print("log目录已存在！！！")
+        except BaseException as msg:
+            print("新建log目录失败：%s" % msg)
         # 如果case组织结构式 /testsuit/featuremodel/xxx.py ， 那么得到的相对路径的父路径就是项目根目录
         log_name = log_path + rq + '.log'
         fh = logging.FileHandler(log_name)
